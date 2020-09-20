46a077679c7b5cca947885c9fdd8b7df9f112245
sai <sais77.code@gmail.com>
2020-09-21 05:06:07 +0530

    Adding

---
+++ Section1/output.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  version = " ~> 2.6 "
+  region = "ap-south-1"
+  access_key = "AWS-Access-Key"
+  secret_key = "AWS-Secret"
+} 
+
+resource "aws_instance" "firstec2" {
+  ami = "ami-09a7bbd08886aafdf"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "First-EC2"
+  }
+}
+
+output "ip" {
+  value = aws_instance.firstec2.public_ip
+}
+
+output "EC2-state" {
+  value = aws_instance.firstec2.instance_state
+}
